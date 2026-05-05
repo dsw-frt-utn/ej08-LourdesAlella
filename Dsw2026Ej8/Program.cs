0d8caaca6627a4ffcf8bb404b6322c9f10ba2576
Lourdes Alella <Lourdes.BrandanAlella@alu.frt.utn.edu.ar>
2026-05-04 21:26:04 -0300

    Problema 2 resuelto

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dsw2026Ej8.Problema1;
+using Dsw2026Ej8.Problema2;
 
 namespace Dsw2026Ej8
 {
@@ -7,13 +8,21 @@ namespace Dsw2026Ej8
         static void Main(string[] args)
         {
             ProbarProblema1();
+            ProbarProblema2();
         }
 
         static void ProbarProblema1()
         {
             Console.WriteLine(" === PROBLEMA 1 === ");
             var helper = new ProductHelper();
-            Console.WriteLine(helper.ObtenerEtiquetaProducto(1, "Producto A", 100.50m));
+            Console.WriteLine(helper.ObtenerEtiquetaProducto(100, "Producto A", 99.50m));
+        }
+
+        static void ProbarProblema2()
+        {
+            Console.WriteLine("\n === PROBLEMA 2 === ");
+            var prueba2 = new Problema2.Problema2();
+            Console.WriteLine(prueba2.CrearResumenVenta(200, "Producto B", 2, 200));
         }
     }
 }
