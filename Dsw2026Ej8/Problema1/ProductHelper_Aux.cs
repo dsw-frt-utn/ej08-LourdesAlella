bcbfce5d2bb01ead38c88a58a54dccaab05ca38d
Lourdes Alella <Lourdes.BrandanAlella@alu.frt.utn.edu.ar>
2026-05-04 21:03:37 -0300

    Problema 1 resuelto

---
+++ Dsw2026Ej8/Problema1/ProductHelper_Aux.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema1
+{
+    public partial class ProductHelper
+    {
+        private const string separator = " - ";
+
+        private string FormatCode(long code)
+        {
+            return $"[{code}]";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Problema1;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProbarProblema1();
+        }
+
+        static void ProbarProblema1()
+        {
+            Console.WriteLine(" === PROBLEMA 1 === ");
+            var helper = new ProductHelper();
+            Console.WriteLine(helper.ObtenerEtiquetaProducto(1, "Producto A", 100.50m));
         }
     }
 }
