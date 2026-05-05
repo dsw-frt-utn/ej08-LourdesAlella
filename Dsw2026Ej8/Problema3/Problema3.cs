95d4c2e721e03d3b15716701ab2e8b1f67ff1a4d
Lourdes Alella <Lourdes.BrandanAlella@alu.frt.utn.edu.ar>
2026-05-04 21:46:48 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3/Problema3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema3
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valorCopia = originalValue;
+            valorCopia++;
+            Product productoCopia = product;
+            productoCopia.SetDescription("Modificada");
+
+            return $"{originalValue}-{valorCopia}-{product.GetDescription()}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Dsw2026Ej8.Problema1;
 using Dsw2026Ej8.Problema2;
+using Dsw2026Ej8.Problema3;
 
 namespace Dsw2026Ej8
 {
@@ -9,6 +10,8 @@ namespace Dsw2026Ej8
         {
             ProbarProblema1();
             ProbarProblema2();
+            ProbarProblema3();
+            
         }
 
         static void ProbarProblema1()
@@ -23,6 +26,14 @@ namespace Dsw2026Ej8
             Console.WriteLine("\n === PROBLEMA 2 === ");
             var prueba2 = new Problema2.Problema2();
             Console.WriteLine(prueba2.CrearResumenVenta(200, "Producto B", 2, 200));
+        }
+
+        static void ProbarProblema3()
+        {
+            Console.WriteLine("\n === PROBLEMA 3 === ");
+            var prueba3 = new Problema3.Problema3();
+            Product productoPrueba = new Product("Producto C");
+            Console.WriteLine(prueba3.CompararCopias(30, productoPrueba));
         }
     }
 }
